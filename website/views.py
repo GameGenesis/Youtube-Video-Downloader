73c654d86d5652c5c8fd8bb02fc1d543f79aa366
GameGenesis <gamegenesisdev@gmail.com>
2022-02-18 14:46:20 -0500

    Add conversion history webpage with login requirement

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
+
+@views.route("/history", methods=["GET", "POST"])
+@login_required
+def history():
+    return render_template("history.html")
