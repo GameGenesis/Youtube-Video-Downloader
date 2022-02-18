01860faebbceaeac800f5c440504b459fc952dba
GameGenesis <gamegenesisdev@gmail.com>
2022-02-18 17:01:58 -0500

    Change location of flask app initialization

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -6,8 +5,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-def create_app():
-    app = Flask(__name__)
+def create_app(app):
     app.config["SECRET_KEY"] = "1234"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
