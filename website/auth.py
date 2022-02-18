be34be769e3a97a22a96fe29a27fdbb4c885f906
GameGenesis <gamegenesisdev@gmail.com>
2022-02-18 13:17:48 -0500

    Allow post methods & request form info on sign-up

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email is invalid.", category="error")
+        elif len(first_name) < 2:
+            flash("First name is invalid. Must contin at least 2 characters.", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match.", category="error")
+        elif len(password1) < 6:
+            flash("Password is invalid. Must contin at least 6 characters.", category="error")
+        else:
+            flash("Account created successfully!", category="success")
+
     return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
