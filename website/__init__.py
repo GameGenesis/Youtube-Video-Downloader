59cf9c00235a17c05ea9a16e9539bfe1346d0b57
GameGenesis <gamegenesisdev@gmail.com>
2022-02-18 14:10:38 -0500

    Add database creation on init

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "1234"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User, Video
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists(f"website/{DB_NAME}"):
+        db.create_all(app=app)
+        print("Created Database!")
