36aa6259d1322620745e7ec3649c53c15fd0f01d
GameGenesis <gamegenesisdev@gmail.com>
2022-02-18 16:23:05 -0500

    Remove pycache files & unnecessary imports

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,6 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-import time
 
 auth = Blueprint("auth", __name__)
 
--- website/models.py
+++ website/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from time import timezone
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
