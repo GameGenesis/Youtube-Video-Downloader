7dcea56853c910b77fcc0bdd5a02dd6073045c9a
GameGenesis <gamegenesisdev@gmail.com>
2022-02-19 19:26:20 -0500

    Add support for mp3 conversion & Implement better error messages

--- website/views.py
+++ website/views.py
@@ -5,6 +5,7 @@ from . import db
 
 from pytube import YouTube
 from pathlib import Path
+import os
 
 views = Blueprint("views", __name__)
 
@@ -16,18 +17,39 @@ def home():
 
         try:
             yt = YouTube(url)
-            yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-            downloads_path = str(Path.home() / "Downloads")
-            yt.download(downloads_path)
-
-            if current_user.is_authenticated:
-                new_video = Video(title=yt.title, url=url, date=date, user_id=current_user.id)
-                db.session.add(new_video)
-                db.session.commit()
-            
-            flash("Video converted successfully!", category="success")
         except Exception:
             flash("Video URL is not valid.", category="error")
+            return render_template("home.html", user=current_user)
+        
+        try:
+            if request.form["convert"] == "mp4":
+                video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+                file_type = "mp4"
+            elif request.form["convert"] == "mp3":
+                video = yt.streams.filter(only_audio=True).get_audio_only()
+                file_type = "mp3"
+
+            print(f"Downloading \"{video.title}\"..")
+            downloads_path = str(Path.home() / "Downloads")
+            video.download(downloads_path)
+        except Exception:
+            flash("Video could not be converted.", category="error")
+            return render_template("home.html", user=current_user)
+
+        try:
+            if file_type == "mp3":
+                file_path = os.path.join(downloads_path, video.default_filename)
+                os.rename(file_path, file_path.replace("mp4", "mp3"))
+        except:
+            flash("Video could not be converted to an MP3 format successfully. File cannot be found or already exists.", category="error")
+            return render_template("home.html", user=current_user)
+
+        if current_user.is_authenticated:
+            new_video = Video(title=yt.title, url=url, date=date, file_type=file_type, user_id=current_user.id)
+            db.session.add(new_video)
+            db.session.commit()
+        
+        flash("Video converted successfully!", category="success")
         
     return render_template("home.html", user=current_user)
 
