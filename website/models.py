f1d4c00e10b36d9b4958c3127b4d49b091df0991
GameGenesis <gamegenesisdev@gmail.com>
2022-02-19 22:54:05 -0500

    Change first_name attribute to name

--- website/models.py
+++ website/models.py
@@ -14,5 +14,5 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    name = db.Column(db.String(150))
     videos = db.relationship("Video")
